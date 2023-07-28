4fb0b1c9c46385136644a5c0f452e793f6f4322b
nomi-san <wuuyi123@gmail.com>
2023-07-28 23:14:43 +0700

    Bump v13.14

--- balance-buff-viewer.js
+++ balance-buff-viewer.js
@@ -1,9 +1,9 @@
 /**
  * @name balance-buff-viewer-plugin
- * @version 13.13.1
+ * @version 13.14.1
  * @description ARAM/URF balance buff viewer plugin for Pengu Loader.
  * @author nomi-san
  * @link https://github.com/nomi-san/balance-buff-viewer
  */
 
-import 'https://cdn.skypack.dev/balance-buff-viewer-plugin@latest?min';
+export * from 'https://cdn.skypack.dev/balance-buff-viewer-plugin?min';
